bf71c908d074d0522252a3484a15b7d67bd10feb
Ccharliiee <Ccharliiee@gitscm.com>
2023-01-30 16:23:35 -0500

    add user data validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :name, presence: true, length: { in: 3..32 }, uniqueness: { case_sensitive: false }
+    validates :email, presence: true, length: { in: 6..105 }, uniqueness: { case_sensitive: false }, 
+    format: { with: VALID_EMAIL_REGEX }
 end
