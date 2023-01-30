33e7742c8b5be6ec23d4c3e79cb33745d5386e34
Ccharliiee <Ccharliiee@gitscm.com>
2023-01-30 18:22:41 -0500

    add user fk to articles;UI updates&bugfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :articles
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :name, presence: true, length: { in: 3..32 }, uniqueness: { case_sensitive: false }
     validates :email, presence: true, length: { in: 6..105 }, uniqueness: { case_sensitive: false }, 
---
+++ db/migrate/20230130225304_addfk4_articles2_user.rb
@@ -0,0 +1,5 @@
+class Addfk4Articles2User < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :articles, :user, foreign_key: true
+  end
+end
