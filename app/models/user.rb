d341b29465e057b6dca0a5e48979c5d75bc75750
Ccharliiee <Ccharliiee@gitscm.com>
2023-02-01 15:34:37 -0500

    Update user table schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
+    before_save {self.email=email.downcase}
     has_many :articles
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :name, presence: true, length: { in: 3..32 }, uniqueness: { case_sensitive: false }
     validates :email, presence: true, length: { in: 6..105 }, uniqueness: { case_sensitive: false }, 
     format: { with: VALID_EMAIL_REGEX }
+    has_secure_password
 end
---
+++ db/migrate/20230201202424_add_password_digest2_users_table.rb
@@ -0,0 +1,5 @@
+class AddPasswordDigest2UsersTable < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :password_digest, :string
+  end
+end
