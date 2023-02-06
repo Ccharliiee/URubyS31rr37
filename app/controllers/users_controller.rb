5f521186f6d9592f1ab943c7999e7b4814085ff7
Ccharliiee <Ccharliiee@gitscm.com>
2023-02-05 21:03:30 -0500

    add edit function for user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,18 +35,19 @@ class UsersController < ApplicationController
     end
   end
 
-  # # PATCH/PUT /users/1 or /users/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @user.update(user_params)
-  #       format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
-  #       format.json { render :show, status: :ok, location: @user }
-  #     else
-  #       format.html { render :edit, status: :unprocessable_entity }
-  #       format.json { render json: @user.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
+  # PATCH/PUT /users/1 or /users/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to user_url(@user) }
+        flash[:notice] = "User #{@user.name} was successfully updated. "
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
   # # DELETE /users/1 or /users/1.json
   # def destroy
